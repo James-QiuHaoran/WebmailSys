1ce6ed99fbd854ba82234a2e1217894f13abb282
James-QiuHaoran <jamesqiu@hku.hk>
2017-10-15 00:03:35 +0800

    update - add email content display

--- handleContentRequest.php
+++ handleContentRequest.php
@@ -12,8 +12,9 @@ $result = mysqli_query($conn, $query) or die('Error! '.mysqli_error($conn));
 
 // fetch the result
 $row = mysqli_fetch_array($result);
-print $row['sender'].'<br>';
-print $row['title'].'<br>';
-print $row['date'].'<br>';
-print $row['content'].'<br>';
+print "<h3>Title: <strong>".$row['title'].'</strong></h3>';
+print "<h4>Sender: <strong>".$row['sender'].'</strong></h4>';
+print "<h4>Date: <strong>".$row['date'].'</strong></h4>';
+print "<p>".$row['content'].'</p>';
+print "<button onclick=\"back()\"><strong>Go Back</strong></button>";
 ?>
--- handleMailboxChange.php
+++ handleMailboxChange.php
@@ -5,9 +5,16 @@ $conn = mysqli_connect('sophia.cs.hku.hk', 'hrqiu', '0414qhr') or die('Error! '.
 // select database
 mysqli_select_db($conn, 'hrqiu') or die('Error! '.mysqli_error($conn));
 
-$query = 'UPDATE emails SET mailbox =\''.$_GET['mailbox'].'\' WHERE emailID =\''.$_GET['emailID'].'\'';
+if ($_GET['mailbox'] == 'trash')
+	$query = 'UPDATE emails SET mailbox = \'trash\' WHERE emailID =\''.$_GET['emailID'].'\'';
+else if ($_GET['mailbox'] == 'delete')
+	$query = 'DELETE FROM emails WHERE emailID=\''.$_GET['emailID'].'\'';
+else if ($_GET['mailbox'] == 'inbox')
+	$query = 'UPDATE emails SET mailbox = \'inbox\' WHERE emailID =\''.$_GET['emailID'].'\'';
+else if ($_GET['mailbox'] == 'important')
+	$query = 'UPDATE emails SET mailbox = \'important\' WHERE emailID =\''.$_GET['emailID'].'\'';
+print $query.'<br>';
 // print $query.'<br>';
-// execute the query
 $result = mysqli_query($conn, $query) or die('Error! '.mysqli_error($conn));
 
 // the result should be 1 if successful
